da819935555bea1b2b8b7e1b3c28185a6c62a28c
Pankaj Kumar <parya209@gmail.com>
2022-02-11 20:39:31 +0530

    modified

--- apidocument.md
+++ apidocument.md
@@ -6,6 +6,60 @@ Rewards=> Gift Cards(Api) and Rewards.
 Starbucks Delivers => Menu Api  and geolocation.
                     =>Add to cart.
                     =>Buy now.
+>>>Page 1
+>>>Find a store
+>>>#working
+>>>local links
+find a store data = http://localhost:5347/store
+find a store wrt cityid [for using in map]= http://localhost:5347/store?city_id=2
+find a store wrt cityname[for using in search bar]=http://localhost:5347/store?city_name=Delhi
+find a store wrt name[for using in search bar]=http://localhost:5347/store?name=Punjabi Bagh
+>>>heroku links
+
+
+>>>Page 2 Menu
+>>>keynote=the api fetch all data from coffee as well as food.we can take data as per our convenience using jsp by fetch call.
+>>>#working
+>>>local links
+Menu data of coffee wrt to its category e.g featured beverage n all =http://localhost:5347/Coffee/3
+Menu data of food wrt to its category e.g featured beverage n all =
+>>>heroku links
+
+>>>Coffee finder
+>>>working
+>>>local links
+http://localhost:5347/ourcoffee/8
+http://localhost:5347/ourcoffee/9
+http://localhost:5347/ourcoffee/10
+>>>heroku links
+
+>>>page 5 {Starbucks delivers }
+>>>Coffee Data
+>>> Working
+>>>local links
+Menu data of coffee wrt to its category e.g featured beverage n all =http://localhost:5347/Coffee/3
+Menu data of food wrt to its category e.g featured beverage n all =
+offers api
+add store api delivery time cost for two ratings:
+>>>heroku links
+
+>>>filters
+>>> Working
+>>>local links
+>>>Type=veg/nonveg
+http://localhost:5347/filter?type=Vegetarian
+>>>ratings like less than and greater than equal to 4
+for below 4 = http://localhost:5347/filter?brate=4
+for above 4 = http://localhost:5347/filter?arate=4
+
+<!-- rewards -->
+gift cards
+
+
+
+
+
+
 
 
 
--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.get('/',(req,res)=>{
 app.get('/store',(req,res)=>{
     let city=Number(req.query.city_id);
     let cityname=req.query.city_name;
+    let name=req.query.name;
     let query={};
-    if(city&&cityname){
+    if(city&&cityname&&name){
         query={city_id:city,
             city_name:cityname}
     }
@@ -24,14 +25,24 @@ app.get('/store',(req,res)=>{
         query={city_id:city}
     }else if(cityname){
         query={city_name:cityname}
+    }else if(name){
+        query={name:name}
     }
     db.collection('store').find(query).toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
     })
 })
-
-// Menu By Coffee errors to be asked
+// coffee finder api
+app.get('/ourcoffee/:id',(req,res)=>{
+    let optid=Number(req.params.id);
+    console.log(">>>>>optid",optid);
+    db.collection('ourcoffee').find({"opt_id":optid}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+// Menu By Coffee 
 app.get('/Coffee/:id',(req,res)=>{
 let categoryID=Number(req.params.id);
 console.log(">>>>>categoryID",categoryID);
@@ -41,9 +52,52 @@ db.collection('coffee').find({"category_id":categoryID}).toArray((err,result)=>{
     res.send(result);
 })
 })
+// filter for coffee
+app.get('/filter',(req,res)=>{
+    let item_type=req.query.type;
+    let brate=Number(req.query.brate);
+    let arate=Number(req.query.arate);
+     if(item_type){
+        query={type:item_type}
+    }else if(brate){
+        query={Ratings:{$lte:brate}}
+       
+    }else if(arate){
+        query={Ratings:{$gte:arate}}
+    }
+    db.collection('coffee').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+
+})
 
 
 // Careers wrt city keyword
+app.get('/jobs',(req,res)=>{
+    let cityId = Number(req.query.city_id);
+    // let cityname = req.query.cityname;
+    let query = {};
+    console.log("cityId>>>>",cityId);
+    if(cityId){
+        query = {
+            city_id:cityId,
+            // city_name:cityname
+        };
+    }
+    // else if(cityId){
+    //     query = {city_id:cityId};
+    // }
+    // else if(cityname){
+    //     query = {city_name:cityname};
+    // }
+
+    db.collection('jobs').find(query).toArray((err,result)=>{
+        if(err) throw err
+        res.send(result);
+    })
+})
+
 
 MongoClient.connect(mongoUrl,(err,connection)=>{
     if(err) throw err;
